5de5e2eaf4671a9e5a24435eb0219c3b6864e486
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2020-04-13 14:58:12 +0200

    add template

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-
-
 const start = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(start);
-
 const writeText = `This is what we know about avocado`;
-
 fs.writeFileSync('./txt/output.txt', writeText);
 // console.log(`${writeText}`);
 
@@ -24,19 +20,32 @@ fs.writeFileSync('./txt/output.txt', writeText);
 // });
 // console.log('This will be first read file');
 
+// ---------- Create  Server
 
-// ---------- Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === '' || pathName === '/overview') {
+  const pathName = req.url;                                        // requesting url 
+  if (pathName === '/' || pathName === '/overview') {
     res.end(`This is overview`)
   } else if (pathName === '/product') {
     res.end(`This is from  product`)
   }
-  res.end(`Hello from server`)
+  else if (pathName === '/api') {
+    res.writeHead(200, {                // server status and json type 
+      'Content-type': 'application/json'
+    });
+    res.end(data)
+  }
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
+    });
+    res.end(`<h1>Page not found</h1>`)
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, () => {
   console.log(`Listening from port `)
-}
-);
+});
